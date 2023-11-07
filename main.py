efa5424bbdd3cab297e0c10e290ccaac06690f81
Hkr14 <cesar14111907@gmail.com>
2023-11-07 16:15:35 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import asyncio
 from configs import Config
@@ -7,45 +6,47 @@ from pyrogram.errors import RPCError, BadRequest
 import logging
 from termcolor import colored
 
-bot = Client(
-    "KimuraBot",
-    api_hash=Config.API_HASH,   
-    api_id=Config.API_ID,
-    bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="plugins"),
+async def main():
+    bot = Client(
+        "Alexcodex",
+        api_hash=Config.API_HASH,
+        api_id=Config.API_ID,
+        bot_token=Config.BOT_TOKEN,
+        plugins=dict(root="plugins"),
     )
 
-@bot.on_callback_query()
-async def callback_privates(client, callback_query):
-  reply_message = callback_query.message.reply_to_message
-  if reply_message is not None and reply_message.from_user is not None:
-    if reply_message.from_user.id != callback_query.from_user.id:
-      await callback_query.answer("❗No tienes acceso ❗")
-      return
-    else:
-      await callback_query.answer("❗No se encontró el mensaje de respuesta o el remitente ❗")
-      return
+    @bot.on_callback_query()
+    async def callback_privates(client, callback_query):
+        reply_message = callback_query.message.reply_to_message
+        if reply_message is not None and reply_message.from_user is not None:
+            if reply_message.from_user.id != callback_query.from_user.id:
+                await callback_query.answer("❗No tienes acceso ❗")
+                return
+        else:
+            await callback_query.answer("❗No se encontró el mensaje de respuesta o el remitente ❗")
+            return
 
         # Inserta aquí tu código específico para manejar las consultas de callback
         # ...
 
-      await callback_query.continue_propagation()
+        await callback_query.continue_propagation()
 
     try:
-      await bot.start()
+        await bot.start()
 
         # Mensaje de inicio con estilo y colores
-      colored_text = colored("Hola amo azuna chk se acaba de iniciar gracias por crearme 💋", "cyan")
-      print(colored_text)
+        colored_text = colored("Hola amo azuna chk se acaba de iniciar gracias por crearme 💋", "cyan")
+        print(colored_text)
 
         # Mantener el bot en funcionamiento
-      while True:
-          await asyncio.sleep(60)  # Esperar 60 segundos antes de volver a verificar eventos
+        while True:
+            await asyncio.sleep(60)  # Esperar 60 segundos antes de volver a verificar eventos
 
-  
+    except Exception as e:
+        print(e)
+    finally:
+        await bot.stop()
+    print('iniciado !')
 
 if __name__ == "__main__":
-	os.system('clear')    
-	print('iniciado Kimura CHK')         
-	bot.run()
-	print('iniciado Kimura CHK')
+    asyncio.run(main())
