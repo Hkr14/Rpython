f6ba8fa0bd0c566a0d6a5348184f9f55f6f6c7f7
Hkr14 <cesar14111907@gmail.com>
2023-11-09 09:47:40 -0700

    update db.py

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import mysql.connector
 try:
   db = mysql.connector.connect(
-    host="185.214.132.8",
-    puerto=
+    host="srv938.hstgr.io",
     user="u943517844_racextpy",
     password="Cesar0728.",
     database="u943517844_racextpy"
