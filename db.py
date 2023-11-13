60d6e885348e0582f41983972f854fd4300692ae
Hkr14 <cesar14111907@gmail.com>
2023-11-13 02:40:35 -0700

    update db.py

--- db.py
+++ db.py
@@ -2,7 +2,4 @@ import mysql.connector
 
 db = mysql.connector.connect( host='185.214.132.8', user= 'u943517844_racextpy', passwd='Cesar0728.', db='u943517844_racextpy' )
 cur = db.cursor()
-cur.execute("SELECT users, FORM id")
-for users, id in cur.fetchall():
-    print users, id
 db.close()
