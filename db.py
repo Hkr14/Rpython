20251e32abbcfd99c02e007f12f89d9a80f2a313
Hkr14 <cesar14111907@gmail.com>
2023-11-13 02:43:30 -0700

    update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import mysql.connector
 
-db = mysql.connector.connect( host='185.214.132.8', user= 'u943517844_racextpy', passwd='Cesar0728.', db='u943517844_racextpy' )
+db = mysql.connector.connect( host='185.214.132.8', user= 'u943517844_racextpy', passwd='Cesar0728.', database='u943517844_racextpy' )
 cur = db.cursor()
 db.close()
