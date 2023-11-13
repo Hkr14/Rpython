d215baa24196b4f7a19bb06ea23c228cfcaff1b6
Hkr14 <cesar14111907@gmail.com>
2023-11-13 02:38:35 -0700

    update db.py

--- db.py
+++ db.py
@@ -1,16 +1,8 @@
-import asyncio
-import aiomysql
+import mysql.connector
 
-
-loop = asyncio.get_event_loop()
-
-async def test_example():
-  conn = await aiomysql.connect(host='185.214.132.8', user='u943517844_racextpy', password='Cesar0728.', db='u943517844_racextpy', loop=loop)
-  cur = await conn.cursor()
-  await cur.execute("SELECT Host,User FROM user")
-  print(cur.description)
-  r = await cur.fetchall()
-  print(r)
-  await cur.close()
-  conn.close()
-loop.run_until_complete(test_example())
+db = mysql.connector.connect( host='185.214.132.8', user= 'u943517844_racextpy', passwd='Cesar0728.', db='u943517844_racextpy' )
+cur = db.cursor()
+cur.execute("SELECT users, FORM id")
+for users, id in cur.fetchall():
+    print users, id
+db.close()
